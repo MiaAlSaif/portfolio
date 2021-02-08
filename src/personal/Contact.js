3f3c7785fafb72a770544bd0557d077a54e6e3e6
Mia AlSaif <miaalsaif@Mias-MacBook-Pro-2.local>
2021-02-08 17:05:57 -0500

    changes

--- src/personal/Contact.js
+++ src/personal/Contact.js
@@ -11,11 +11,11 @@ const Contact = () => (
         <p className="name">Manal AlSaif</p>
         <p>Product Designer</p>
         <p>
-          <span>Email</span> mia.alsaif@gmail.com
+          <span>Email</span> mia.alsaif[@]gmail.com
         </p>
         <p>
           <span>LinkedIn</span>{" "}
-          <ExternalLink href="www.linkedin.com/in/miaalsaif" />
+          <a href="www.linkedin.com/in/miaalsaif">linkedin.com/in/miaalsaif</a>
           {/* <a href="www.linkedin.com/in/miaalsaif" target="_blank">
             linkedin.com/in/miaalsaif
           </a> */}
